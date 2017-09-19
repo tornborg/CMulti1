82a55952c4de027b83dc3e0546e39a4de353fe4b
Axel Isberg <axelisberg@Axels-MacBook-Pro.local>
2017-09-19 16:14:00 +0200

    lekt med trådar

--- cmake-build-debug/sort.c
+++ cmake-build-debug/sort.c
@@ -8,18 +8,30 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+typedef struct {
+    double* list;
+    int n;
+    int s;
+    int threads;
+}arguments;
+
 static double sec(void)
 {
     return clock();
 }
 
+
+
 void par_sort(
                   void*		base,	// Array to sort.
                   size_t		n,	// Number of elements in base.
                   size_t		s,	// Size of each element.
-                  int		(*cmp)(const void*, const void*), int threads) // Behaves like strcmp
+                  int		(*cmp)(const void*, const void*),
+                  int threads) // Behaves like strcmp
 {   int pivot = sizeof(base) - 1;
     int storeIndex = pivot - 1;
+    pthread_t thread0;
+    pthread_t thread1;
     double* unsorted = (double*) base;
     double temp;
       for(int i = 0; i < n; i++){
@@ -43,16 +55,36 @@ void par_sort(
                 upper[i] = unsorted[i];
             }
         }
+
+
+        arguments* args1 = malloc(sizeof(arguments));
+        arguments* args2 = malloc(sizeof(arguments));
+
+        args1->list = malloc(sizeof(lower));
+        args1->list = lower;
+
+        args1->n = storeIndex;
+        args1->s = s;
+        args1->threads = threads;
+
+        args2->list = upper;
+        args2.n = n-storeIndex;
+        args2.s = s;
+        args2.cmp = cmp;
+        args2.threads = threads;
+
         if (threads < 1) {
             threads +=2;
-            pthread_create(par_sort(lower, storeIndex, s, cmp, threads));
-            pthread_create(par_sort(upper, n-storeIndex, s, cmp, threads));
+
+            pthread_create(&thread0, NULL, par_sort, &args1);
+            pthread_create(&thread1, NULL, par_sort, &args2);
         } else {
             threads++;
-            pthread_create(par_sort(lower, storeIndex, s, cmp, threads));
+            pthread_create(&thread0, NULL, par_sort, &args1);
             qsort(upper, n, s, cmp);
     }
-        pthread_join;
+        pthread_join(thread0, NULL);
+        pthread_join(thread1, NULL);
     } else{
             qsort(unsorted, n, s, cmp);
         }
@@ -62,11 +94,15 @@ void par_sort(
 
 }
 
+void par_thread(void* argum){
+    arguents* things = argum;
+    things = (arguments) argum;
+    par_sort(things->list, things->n, things->s, things->cmp, things->threads);
+}
+
 static int cmp(const void* ap, const void* bp)
 {
-    if(ap > bp) return 1;
-    else if (bp > ap) return -1;
-    return 0;
+    return (*(double*)ap) - *((double*) bp);
 }
 
 
@@ -77,6 +113,7 @@ int main(int ac, char** av)
     double*		a;
     double		start, end;
     int threadscreated = 0;
+
 
 
     if (ac > 1)
@@ -99,7 +136,8 @@ int main(int ac, char** av)
     end = sec();
 
     printf("%1.2f s\n", (end - start)/CLOCKS_PER_SEC);
-
+    for(int i = 0; i<50; i++)
+        printf("%1.2f s\n", a[i]);
     free(a);
 
     return 0;
